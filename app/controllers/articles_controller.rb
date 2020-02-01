e912a10908ef32088018e68f9b05329b97c73983
Randolph Cisneros <cisnerosRandolph@gmail.com>
2020-02-01 15:27:51 -0500

    Add create and delete methods, template

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,5 +6,24 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        @article.save
+        redirect_to article_path(@article)
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to "/articles#index"
+    end
     
+    def article_params
+        params.require(:article).permit(:title, :body)
+    end
 end
