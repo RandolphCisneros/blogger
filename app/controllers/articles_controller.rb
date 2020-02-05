449d3afd2f4ddad58c2bbafdfa71ca680de4830a
Randolph Cisneros <cisnerosRandolph@gmail.com>
2020-02-04 20:28:09 -0500

    Blogger change root, add flash notice

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,14 +14,32 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         @article.save
+
+        flash.notice = "Article '#{@article.title}' Saved!"
         redirect_to article_path(@article)
     end
 
     def destroy
         @article = Article.find(params[:id])
         @article.destroy
-        redirect_to "/articles#index"
+
+        flash.notice = "Article #{@article.title} Deleted"
+
+        redirect_to articles_path
     end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+
+        flash.notice = "Article '#{@article.title}' Updated!"
+      
+        redirect_to article_path(@article)
+      end
     
     def article_params
         params.require(:article).permit(:title, :body)
