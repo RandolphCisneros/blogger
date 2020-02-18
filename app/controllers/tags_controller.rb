dfab7908d025bf73de7475920ccc383ec231cb2c
Randolph Cisneros <cisnerosRandolph@gmail.com>
2020-02-17 20:37:57 -0500

    blogger add tags

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,22 @@
+class TagsController < ApplicationController
+    def index
+        @tags = Tag.all
+    end
+
+    def show
+        @tag = Tag.find(params[:id])
+    end
+
+    def destroy
+        @tag = Tag.find(params[:id])
+        @tag.destroy
+
+        flash.notice = "Tag #{@tag.name} Deleted"
+
+        redirect_to tags_path
+    end
+
+    def tag_params
+        params.require(:tag).permit(:name, :article_list)
+    end
+end
