0311234cb951f09ee152cea77343cd67b88d83cd
Randolph Cisneros <cisnerosRandolph@gmail.com>
2020-02-18 21:50:58 -0500

    Blogger add paperclip

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -45,6 +45,6 @@ class ArticlesController < ApplicationController
       end
     
     def article_params
-        params.require(:article).permit(:title, :body, :tag_list)
+        params.require(:article).permit(:title, :body, :tag_list, :image)
     end
 end
